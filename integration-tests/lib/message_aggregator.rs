1a95a2e55b1e89b60daba7b106ba50596f7cc6fd
bit-aloo <sshourya17@gmail.com>
2026-07-21 19:31:01 +0530

    replace custom mutex with shared primitive in message aggregator

--- integration-tests/lib/message_aggregator.rs
+++ integration-tests/lib/message_aggregator.rs
@@ -1,7 +1,7 @@
-use std::{collections::VecDeque, sync::Arc};
+use std::collections::VecDeque;
 use stratum_apps::{
-    custom_mutex::Mutex,
     stratum_core::parsers_sv2::{AnyMessage, Tlv},
+    sync::SharedLock,
 };
 
 use crate::types::MsgType;
@@ -9,7 +9,7 @@ use crate::types::MsgType;
 #[allow(clippy::type_complexity)]
 #[derive(Debug, Clone)]
 pub struct MessagesAggregator {
-    messages: Arc<Mutex<VecDeque<(MsgType, AnyMessage<'static>, Option<Vec<Tlv>>)>>>,
+    messages: SharedLock<VecDeque<(MsgType, AnyMessage<'static>, Option<Vec<Tlv>>)>>,
 }
 
 impl Default for MessagesAggregator {
@@ -22,7 +22,7 @@ impl MessagesAggregator {
     /// Creates a new [`MessagesAggregator`].
     pub fn new() -> Self {
         Self {
-            messages: Arc::new(Mutex::new(VecDeque::new())),
+            messages: SharedLock::new(VecDeque::new()),
         }
     }
 
@@ -39,29 +39,25 @@ impl MessagesAggregator {
         tlv_fields: Option<Vec<Tlv>>,
     ) {
         self.messages
-            .safe_lock(|messages| messages.push_back((msg_type, message, tlv_fields)))
+            .with(|messages| messages.push_back((msg_type, message, tlv_fields)))
             .unwrap();
     }
 
     /// Returns false if the queue is empty, true otherwise.
     pub fn is_empty(&self) -> bool {
-        self.messages
-            .safe_lock(|messages| messages.is_empty())
-            .unwrap()
+        self.messages.with(|messages| messages.is_empty()).unwrap()
     }
 
     /// Clears all messages from the queue.
     pub fn clear(&self) {
-        self.messages
-            .safe_lock(|messages| messages.clear())
-            .unwrap();
+        self.messages.with(|messages| messages.clear()).unwrap();
     }
 
     /// returns true if contains message_type
     pub fn has_message_type(&self, message_type: u8) -> bool {
         let has_message: bool = self
             .messages
-            .safe_lock(|messages| {
+            .with(|messages| {
                 for (t, _, _) in messages.iter() {
                     if *t == message_type {
                         return true; // Exit early with `true`
@@ -77,7 +73,7 @@ impl MessagesAggregator {
     /// until the first message of type message_type.
     pub fn has_message_type_with_remove(&self, message_type: u8) -> bool {
         self.messages
-            .safe_lock(|messages| {
+            .with(|messages| {
                 let mut cloned_messages = messages.clone();
                 for (pos, (t, _, _)) in cloned_messages.iter().enumerate() {
                     if *t == message_type {
@@ -109,7 +105,7 @@ impl MessagesAggregator {
     ) -> Option<(MsgType, AnyMessage<'static>, Option<Vec<Tlv>>)> {
         let is_state = self
             .messages
-            .safe_lock(|messages| {
+            .with(|messages| {
                 let mut cloned = messages.clone();
                 if let Some((msg_type, msg, tlv_fields)) = cloned.pop_front() {
                     *messages = cloned;
