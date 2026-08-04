2950616d297392d66d3d4ddf6e57b9ac4eea6760
bit-aloo <sshourya17@gmail.com>
2026-08-04 11:46:52 +0530

    update translator to use new error associate dtype from sv1 server trait

--- miner-apps/translator/src/lib/sv1/sv1_server/downstream_message_handler.rs
+++ miner-apps/translator/src/lib/sv1/sv1_server/downstream_message_handler.rs
@@ -1,14 +1,12 @@
 use stratum_apps::stratum_core::sv1_api::{
-    IsServer, client_to_server,
-    error::Error,
-    json_rpc,
+    IsServer, client_to_server, json_rpc,
     server_to_client::{self, Notify},
     utils::{Extranonce, HexU32Be},
 };
-use tracing::{debug, info, warn};
+use tracing::{debug, error, info, warn};
 
 use crate::{
-    error,
+    error::TproxyErrorKind,
     sv1::Sv1Server,
     utils::{
         AGGREGATED_CHANNEL_ID, SubmitShareWithChannelId, sv1_worker_name_from_sv1_username,
@@ -19,18 +17,20 @@ use crate::{
 // Implements `IsServer` for `Sv1Server` to handle the Sv1 messages.
 #[cfg_attr(not(test), hotpath::measure_all)]
 impl IsServer for Sv1Server {
+    type Error = TproxyErrorKind;
+
     fn handle_configure(
         &mut self,
         client_id: Option<usize>,
         request: &client_to_server::Configure,
-    ) -> Result<(Option<server_to_client::VersionRollingParams>, Option<bool>), Error> {
+    ) -> Result<(Option<server_to_client::VersionRollingParams>, Option<bool>), Self::Error> {
         let downstream_id = client_id.expect("Downstream id should exist");
 
         info!("Received mining.configure from SV1 downstream");
         debug!("Downstream {downstream_id}: mining.configure = {}", request);
 
         let Some(downstream) = self.downstreams.get(&downstream_id) else {
-            return Err(Error::UnknownID(downstream_id as u64));
+            return Err(TproxyErrorKind::DownstreamNotPresent(downstream_id));
         };
 
         downstream.downstream_data.super_safe_lock(|data| {
@@ -62,7 +62,7 @@ impl IsServer for Sv1Server {
         &self,
         client_id: Option<usize>,
         request: &client_to_server::Subscribe,
-    ) -> Result<Vec<(String, String)>, Error> {
+    ) -> Result<Vec<(String, String)>, Self::Error> {
         let downstream_id = client_id.expect("Downstream id should exist");
 
         info!("Received mining.subscribe from Sv1 downstream");
@@ -85,7 +85,7 @@ impl IsServer for Sv1Server {
         &self,
         client_id: Option<usize>,
         request: &client_to_server::Authorize,
-    ) -> Result<bool, Error> {
+    ) -> Result<bool, Self::Error> {
         let downstream_id = client_id.expect("Downstream id should exist");
         info!("Received mining.authorize from Sv1 downstream {downstream_id}");
         debug!("Down: Handling mining.authorize: {}", request);
@@ -96,11 +96,11 @@ impl IsServer for Sv1Server {
         &self,
         client_id: Option<usize>,
         request: &client_to_server::Submit,
-    ) -> Result<bool, Error> {
+    ) -> Result<bool, Self::Error> {
         let downstream_id = client_id.expect("Downstream id should exist");
 
         let Some(downstream) = self.downstreams.get(&downstream_id) else {
-            return Err(Error::UnknownID(downstream_id as u64));
+            return Err(TproxyErrorKind::DownstreamNotPresent(downstream_id));
         };
 
         let job_id = &request.job_id;
@@ -175,15 +175,15 @@ impl IsServer for Sv1Server {
     }
 
     /// Indicates to the server that the client supports the mining.set_extranonce method.
-    fn handle_extranonce_subscribe(&self) -> Result<(), Error> {
+    fn handle_extranonce_subscribe(&self) -> Result<(), Self::Error> {
         Ok(())
     }
 
     /// Checks if a Downstream role is authorized.
-    fn is_authorized(&self, client_id: Option<usize>, name: &str) -> Result<bool, Error> {
+    fn is_authorized(&self, client_id: Option<usize>, name: &str) -> Result<bool, Self::Error> {
         let downstream_id = client_id.expect("Downstream id should exist");
         let Some(downstream) = self.downstreams.get(&downstream_id) else {
-            return Err(Error::UnknownID(downstream_id as u64));
+            return Err(TproxyErrorKind::DownstreamNotPresent(downstream_id));
         };
         let is_authorized = downstream
             .downstream_data
@@ -192,10 +192,10 @@ impl IsServer for Sv1Server {
     }
 
     /// Authorizes a Downstream role.
-    fn authorize(&mut self, client_id: Option<usize>, name: &str) -> Result<(), Error> {
+    fn authorize(&mut self, client_id: Option<usize>, name: &str) -> Result<(), Self::Error> {
         let downstream_id = client_id.expect("Downstream id should exist");
         let Some(downstream) = self.downstreams.get(&downstream_id) else {
-            return Err(Error::UnknownID(downstream_id as u64));
+            return Err(TproxyErrorKind::DownstreamNotPresent(downstream_id));
         };
 
         let is_authorized = self.is_authorized(client_id, name)?;
@@ -218,10 +218,10 @@ impl IsServer for Sv1Server {
         &mut self,
         client_id: Option<usize>,
         _extranonce1: Option<Extranonce>,
-    ) -> Result<Extranonce, Error> {
+    ) -> Result<Extranonce, Self::Error> {
         let downstream_id = client_id.expect("Downstream id should exist");
         let Some(downstream) = self.downstreams.get(&downstream_id) else {
-            return Err(Error::UnknownID(downstream_id as u64));
+            return Err(TproxyErrorKind::DownstreamNotPresent(downstream_id));
         };
         downstream
             .downstream_data
@@ -229,10 +229,10 @@ impl IsServer for Sv1Server {
     }
 
     /// Returns the `Downstream`'s `extranonce1` value.
-    fn extranonce1(&self, client_id: Option<usize>) -> Result<Extranonce, Error> {
+    fn extranonce1(&self, client_id: Option<usize>) -> Result<Extranonce, Self::Error> {
         let downstream_id = client_id.expect("Downstream id should exist");
         let Some(downstream) = self.downstreams.get(&downstream_id) else {
-            return Err(Error::UnknownID(downstream_id as u64));
+            return Err(TproxyErrorKind::DownstreamNotPresent(downstream_id));
         };
         downstream
             .downstream_data
@@ -245,10 +245,10 @@ impl IsServer for Sv1Server {
         &mut self,
         client_id: Option<usize>,
         _extra_nonce2_size: Option<usize>,
-    ) -> Result<usize, Error> {
+    ) -> Result<usize, Self::Error> {
         let downstream_id = client_id.expect("Downstream id should exist");
         let Some(downstream) = self.downstreams.get(&downstream_id) else {
-            return Err(Error::UnknownID(downstream_id as u64));
+            return Err(TproxyErrorKind::DownstreamNotPresent(downstream_id));
         };
         downstream
             .downstream_data
@@ -256,10 +256,10 @@ impl IsServer for Sv1Server {
     }
 
     /// Returns the `Downstream`'s `extranonce2_size` value.
-    fn extranonce2_size(&self, client_id: Option<usize>) -> Result<usize, Error> {
+    fn extranonce2_size(&self, client_id: Option<usize>) -> Result<usize, Self::Error> {
         let downstream_id = client_id.expect("Downstream id should exist");
         let Some(downstream) = self.downstreams.get(&downstream_id) else {
-            return Err(Error::UnknownID(downstream_id as u64));
+            return Err(TproxyErrorKind::DownstreamNotPresent(downstream_id));
         };
         downstream
             .downstream_data
@@ -267,10 +267,13 @@ impl IsServer for Sv1Server {
     }
 
     /// Returns the version rolling mask.
-    fn version_rolling_mask(&self, client_id: Option<usize>) -> Result<Option<HexU32Be>, Error> {
+    fn version_rolling_mask(
+        &self,
+        client_id: Option<usize>,
+    ) -> Result<Option<HexU32Be>, Self::Error> {
         let downstream_id = client_id.expect("Downstream id should exist");
         let Some(downstream) = self.downstreams.get(&downstream_id) else {
-            return Err(Error::UnknownID(downstream_id as u64));
+            return Err(TproxyErrorKind::DownstreamNotPresent(downstream_id));
         };
         downstream
             .downstream_data
@@ -282,10 +285,10 @@ impl IsServer for Sv1Server {
         &mut self,
         client_id: Option<usize>,
         mask: Option<HexU32Be>,
-    ) -> Result<(), Error> {
+    ) -> Result<(), Self::Error> {
         let downstream_id = client_id.expect("Downstream id should exist");
         let Some(downstream) = self.downstreams.get(&downstream_id) else {
-            return Err(Error::UnknownID(downstream_id as u64));
+            return Err(TproxyErrorKind::DownstreamNotPresent(downstream_id));
         };
 
         downstream
@@ -300,10 +303,10 @@ impl IsServer for Sv1Server {
         &mut self,
         client_id: Option<usize>,
         mask: Option<HexU32Be>,
-    ) -> Result<(), Error> {
+    ) -> Result<(), Self::Error> {
         let downstream_id = client_id.expect("Downstream id should exist");
         let Some(downstream) = self.downstreams.get(&downstream_id) else {
-            return Err(Error::UnknownID(downstream_id as u64));
+            return Err(TproxyErrorKind::DownstreamNotPresent(downstream_id));
         };
         downstream
             .downstream_data
@@ -311,12 +314,13 @@ impl IsServer for Sv1Server {
         Ok(())
     }
 
-    fn notify(&'_ mut self, _client_id: Option<usize>) -> Result<json_rpc::Message, Error> {
+    fn notify(&mut self, _client_id: Option<usize>) -> Result<json_rpc::Message, Self::Error> {
         warn!("notify() called on Sv1Server - this method is not implemented for Sv1Server");
         Err(
             stratum_apps::stratum_core::sv1_api::error::Error::UnexpectedMessage(
                 "notify".to_string(),
-            ),
+            )
+            .into(),
         )
     }
 }
